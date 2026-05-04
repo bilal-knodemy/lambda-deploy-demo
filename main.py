b7487344f04fb29584ceb3560c096f2cfc476d86
bilal-knodemy <bilal@knodemy.com>
2026-05-04 19:11:51 +0500

    refactor main

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ app = FastAPI()
 def home():
     return {"message": "API is working fine 🚀"}
 
+
 @app.get("/user")
 def get_user():
     data = {"name": "Ali"}
 
     if "age" not in data:
-        raise Exception("Unexpected server error: age missing")
+        # This forces a real server error (500)
+        raise Exception("Age not found in data")
 
-    return {"name": data["name"], "age": data["age"]}
+    return {
+        "name": data["name"],
+        "age": data["age"]
+    }
 
 handler = Mangum(app)
